9018191cbcf5b2560ae2a2c0d25a7d8facb84ca5
Karthick <karthickbharathi15@gmail.com>
2023-11-11 23:43:17 +0530

    update

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -11,36 +11,50 @@ export default function EditPost() {
   const [redirect, setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch("https://blogie-back-end.onrender.com/post/" + id).then(
-      (response) => {
-        response.json().then((postInfo) => {
-          console.log(postInfo);
-          setTitle(postInfo.title);
-          setContent(postInfo.content);
-          setFiles(postInfo.cover);
-          setSummary(postInfo.summary);
-        });
-      }
-    );
-  }, []);
+    fetch(`https://blogie-back-end.onrender.com/post/${id}`)
+      .then((response) => response.json())
+      .then((postInfo) => {
+        console.log(postInfo);
+        setTitle(postInfo.title);
+        setContent(postInfo.content);
+        setFiles(postInfo.cover);
+        setSummary(postInfo.summary);
+      })
+      .catch((error) => {
+        console.error("Error fetching post:", error);
+      });
+  }, [id]);
 
-  async function updatePost(ev) {
-    ev.preventDefault();
+  async function updatePost(e) {
+    e.preventDefault();
+
     const data = new FormData();
     data.set("title", title);
     data.set("summary", summary);
     data.set("content", content);
     data.set("id", id);
+
     if (files?.[0]) {
       data.set("file", files?.[0]);
     }
-    const response = await fetch("https://blogie-back-end.onrender.com/post", {
-      method: "PUT",
-      body: data,
-      credentials: "include",
-    });
-    if (response.ok) {
-      setRedirect(true);
+
+    try {
+      const response = await fetch(
+        `https://blogie-back-end.onrender.com/post`,
+        {
+          method: "PUT",
+          body: data,
+          credentials: "include",
+        }
+      );
+
+      if (response.ok) {
+        setRedirect(true);
+      } else {
+        console.error(`Failed to update post. Status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("Error updating post:", error);
     }
   }
 
@@ -51,18 +65,18 @@ export default function EditPost() {
   return (
     <form onSubmit={updatePost} className="edit-post">
       <input
-        type="title"
-        placeholder={"Title"}
+        type="text"
+        placeholder="Title"
         value={title}
-        onChange={(ev) => setTitle(ev.target.value)}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <input
-        type="summary"
-        placeholder={"Summary"}
+        type="text"
+        placeholder="Summary"
         value={summary}
-        onChange={(ev) => setSummary(ev.target.value)}
+        onChange={(e) => setSummary(e.target.value)}
       />
-      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
       <Editor onChange={setContent} value={content} />
       <button style={{ marginTop: "5px" }}>Update post</button>
     </form>
