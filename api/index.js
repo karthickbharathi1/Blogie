1f0e524da0a8d3327f32d326a400b095ddd3eaf6
Karthick <karthickbharathi15@gmail.com>
2023-11-13 18:57:57 +0530

    update

--- api/index.js
+++ api/index.js
@@ -73,6 +73,7 @@ app.post("/login", async (req, res) => {
     // logged in
     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
+      console.log(token);
       res.cookie("token", token).json({
         id: userDoc._id,
         username,
